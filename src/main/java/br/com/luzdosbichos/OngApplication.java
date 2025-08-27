ab5c2c509d277f807b6b3f9a3f3ae01cf037613c
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2025-08-27 17:24:12 -0300

    Initialize Spring Boot project with Gradle setup and basic configuration

---
+++ src/main/java/br/com/luzdosbichos/OngApplication.java
@@ -0,0 +1,13 @@
+package br.com.luzdosbichos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OngApplication.class, args);
+    }
+
+}
