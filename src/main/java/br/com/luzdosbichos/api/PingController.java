cce7b61af62b55c5288f1c0fd8fe1270cd004a20
Joana Elias de Oliveira <joana.oliverlias@outlook.com>
2025-09-06 18:31:49 -0300

    teste deploy automatuico

--- src/main/java/br/com/luzdosbichos/api/PingController.java
+++ src/main/java/br/com/luzdosbichos/api/PingController.java
@@ -21,6 +21,7 @@ public class PingController {
                 "service", "aumigos-api",
                 "buildSha", buildSha,
                 "tetttffet", "teste deploy",
+                "jo linda", "teste vinicius dunnciondandji",
                 "buildTime", buildTime
         );
     }
