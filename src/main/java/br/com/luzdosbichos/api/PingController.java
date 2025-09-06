b1d23d2133bd6acf5bb72ee4f4f1b01d1149a706
Joana Elias de Oliveira <joana.oliverlias@outlook.com>
2025-09-06 18:09:10 -0300

    teste deploy automatuico

--- src/main/java/br/com/luzdosbichos/api/PingController.java
+++ src/main/java/br/com/luzdosbichos/api/PingController.java
@@ -1,13 +1,26 @@
 package br.com.luzdosbichos.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
 public class PingController {
+
+    @Value("${APP_BUILD_SHA:unknown}")
+    private String buildSha;
+
+    @Value("${APP_BUILD_TIME:unknown}")
+    private String buildTime;
+
     @GetMapping("/ping")
     public Map<String, Object> ping() {
-        return Map.of("ok", true, "service", "aumigos-api");
+        return Map.of(
+                "ok", true,
+                "service", "aumigos-api",
+                "buildSha", buildSha,
+                "buildTime", buildTime
+        );
     }
 }
