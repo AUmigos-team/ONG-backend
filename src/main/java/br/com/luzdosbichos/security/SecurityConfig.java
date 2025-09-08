1ac03c2ffbedb46bbdbdad03f5366c164856711e
Joana Elias de Oliveira <joana.oliverlias@outlook.com>
2025-09-08 12:54:43 -0300

    questoes de corss

--- src/main/java/br/com/luzdosbichos/security/SecurityConfig.java
+++ src/main/java/br/com/luzdosbichos/security/SecurityConfig.java
@@ -1,10 +1,15 @@
-package br.com.luzdosbichos.config;
+package br.com.luzdosbichos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -26,8 +31,26 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable());
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults());
 
         return http.build();
     }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        var c = new CorsConfiguration();
+        c.setAllowedOrigins(List.of(
+                "https://app.santuarioluzdosbichos.org",
+                "http://localhost:4200"
+        ));
+        c.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
+        c.setAllowedHeaders(List.of("*"));
+        c.setAllowCredentials(true);
+
+        var s = new UrlBasedCorsConfigurationSource();
+        s.registerCorsConfiguration("/**", c);
+        return s;
+    }
+
 }
