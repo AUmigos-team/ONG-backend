9c0465edbb9e625bc10eb5c81a4126b4a5f52083
Eduardo Soares <eduardo_soares001@outlook.com>
2025-09-09 15:16:05 -0300

    Implements enums class

---
+++ src/main/java/br/com/luzdosbichos/model/animal/enums/Gender.java
@@ -0,0 +1,6 @@
+package br.com.luzdosbichos.model.animal.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE;
+}
---
+++ src/main/java/br/com/luzdosbichos/model/animal/enums/TypeAnimal.java
@@ -0,0 +1,7 @@
+package br.com.luzdosbichos.model.animal.enums;
+
+public enum TypeAnimal {
+    DOG,
+    CAT,
+    OTHER;
+}
